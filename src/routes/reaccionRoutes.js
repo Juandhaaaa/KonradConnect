75e7039771c25de681404b090ed1b9a33c8b11a3
juan <juansega96@gmail.com>
2025-10-31 13:55:39 -0500

    ya no marca error, era porque habian dos archivos con el mismo nombre, lo unico que los diferenciaba era una mayuscula, mejor se cambio el nombre

--- src/routes/reaccionRoutes.js
+++ src/routes/reaccionRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Reaccion = require("../models/Reaccion");
+const Reaccion = require("../models/Reacciones");
 
 
 // Crear reacción
